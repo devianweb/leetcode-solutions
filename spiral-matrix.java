b34d4ec922b6150ccd8fcfaa0748a85b18d4d0d3
Ian Webster <87659522+devianweb@users.noreply.github.com>
2025-07-22 20:19:38 +0100

    Update spiral-matrix.java

--- spiral-matrix.java
+++ spiral-matrix.java
@@ -2,25 +2,44 @@ class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length; 
-        
-        System.out.println("m: " + m);
-        System.out.println("n: " + n);
-
         int offset = 0;
         int i = 0;
         int j = 0;
-        while(m > 0 || n > 0) {
-            System.out.println("---while---");
-            while (i < m - offset) {
-                System.out.println("j: " + j + " i: " + i);
-                if (i != m - offset - 1) i++;
-                else break;
+        List<Integer> result = new ArrayList();
+
+        while(m - offset > 0 || n - offset > 0) {
+            for( ; i < n - offset && notFull(m, n, result); i++) {
+                result.add(matrix[j][i]);
             }
 
-            System.out.println("------");
-            System.out.println("j: " + j + " i: " + i);
+            i--;
+            j++;
+            for( ; j < m - offset && notFull(m, n, result); j++) {
+                result.add(matrix[j][i]);
+            }
+
+            j--;
+            i--;
+            for( ; i >= 0 + offset && notFull(m, n, result); i--) {
+                result.add(matrix[j][i]);
+            }
+
+            offset++;
+
+            i++;
+            j--;
+            for( ; j >= 0 + offset && notFull(m, n, result); j--) {
+                result.add(matrix[j][i]);
+            }
+
+            j++;
+            i++;
         }
 
-        return null;
+        return result;
+    }
+
+    private boolean notFull(int m, int n, List<Integer> list) {
+        return list.size() !=  m * n;
     }
 }
