1f138cfa6a1833c44785ae882fc6036196697f48
Ian Webster <87659522+devianweb@users.noreply.github.com>
2025-07-28 10:09:48 +0100

    Update longest-palindromic-substring.java
    removed logging

--- longest-palindromic-substring.java
+++ longest-palindromic-substring.java
@@ -1,7 +1,6 @@
 class Solution {
     public String longestPalindrome(String s) {
         var window = s.length();
-        System.out.println("window: " + window);
         String result = null;
         while(result == null && window != 0) {
             for (var i = 0; i <= s.length() - window; i++) {
